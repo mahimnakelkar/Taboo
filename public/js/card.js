5677e0f03c8f42c2fc80def96dbbbb382ba914df
Mahimna Kelkar <mkelkar@purdue.edu>
2016-11-22 17:57:26 -0500

    Minor Enhancements

--- public/js/card.js
+++ public/js/card.js
@@ -1,4 +1,4 @@
-function insertCard() {
+function insertCard(color, clues) {
 	var container = document.createElement('div');
 	container.setAttribute('class', 'flip-container');
 
@@ -12,19 +12,32 @@ function insertCard() {
 
 	var frontCard = document.createElement('div');
 	frontCard.setAttribute('class', 'card');
+	frontCard.style.backgroundColor = color;
+
 	var backCard = document.createElement('div');
 	backCard.setAttribute('class', 'card');
+	backCard.style.backgroundColor = color;
 
-	frontCard.innerHTML = "Clues Here";
+	for(var i = 0; i < clues.length; i++) {
+		var clue = document.createElement('div');
+		clue.setAttribute('class','clue');
+		clue.innerHTML = clues[i];
+		clue.style.lineHeight = 220/clues.length + 'px';
+		frontCard.appendChild(clue);
+	}
 	front.appendChild(frontCard);
+	
 
-	backCard.innerHTML = "Input Box Here";
-	front.appendChild(frontCard);
-
-
+	var input = document.createElement('div');
+	input.innerHTML = "<center> <input type = 'text' class='answerInput'/> </center>";
+	input.style.lineHeight = '220px';
+	backCard.appendChild(input);
 	back.appendChild(backCard);
+
 	flip.appendChild(front);
 	flip.appendChild(back);
+
+
 	container.appendChild(flip);
 
 	// Change this later to append to the container div for all cards instead of body
