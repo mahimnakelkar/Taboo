2cbd5b31df8d339658e154e50d08ceee5bb38ab5
Mahimna Kelkar <mkelkar@purdue.edu>
2016-11-27 18:44:14 -0500

    Card List using Angular

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,17 +3,66 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <main>
 
-        	<div class="jumbotron">
-        		<h1> Hello World!! </h1>
+    	
+    	<header>
+    		<nav class ="navbar navbar-inverse">
+    			<div class = "navbar-header">			
+    					<span>
+    						<a href= "/" class="navbar-brand"> Taboo </a>
+    					</span>
+    					<span>
+    						<a href= "/" class="navbar-brand login" style> Login </a>
+    					</span>
+    			</div>
+    		</nav>
+    	</header>
+        <main>
+        	<button (click) = "addCard()"> Add Card </button> <p>
+        	<div *ngFor = "let card of cards">
+	        	<div class = "flip-container">
+	        		<div class = "flipper">
+	        			<div class = "front" >
+	        				<div class = "card" [style.background-color]="getColor(card)">
+	        					<div class = "clue">
+	        						<div *ngFor = "let hint of card.hints">
+	        							<span class="hint"> {{ hint }} </span>
+	        						</div>
+	        					</div>
+	        				</div>
+
+	        			</div>
+	        			<div class = "back">
+	        				<div class = "card" [style.background-color]="getColor(card)">
+	        					<div class="inputDiv">
+	        						<input type = 'text' class='answerInput'/>
+	        					</div>
+	        				</div>
+
+	        			</div>
+	        		</div>
+	        	</div>
         	</div>
+
 
 
         </main>
     `
 })
 export class AppComponent {
+	
+	// Connect this part to database later
+	 cards = [
+	 	{color:"green", hints:["Clue1","Clue2"]},
+	 	{color:"red", hints:["Clue1"]},
+	 	{color:"blue", hints:["Clue1", "Clue2", "Clue3","Clue4"]}
+	 ];
 
+	 getColor(card = {color:"green"}) {
+	 	return card.color;
+	 }
 
+	 addCard() {
+	 	this.cards.push({color:"green",hints:["Clue1","Clue2"]});
+	 }
 }
