92e4191d454960f92f041ec9c026f3c53a4bfada
Aviral Mansingka <aviralmansingka@gmail.com>
2016-11-28 14:49:37 -0500

    Added color to card schema

--- models/card.js
+++ models/card.js
@@ -5,7 +5,8 @@ var CardSchema = new Schema({
     lat     : Number,
     lon     : Number,
     answer  : String,
-    hints   : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Hint' }]
+    hints   : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Hint' }],
+    color   : String
 });
 
 module.exports = mongoose.model('Card', CardSchema);
